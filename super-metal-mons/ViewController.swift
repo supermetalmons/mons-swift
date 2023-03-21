290d78eb6c5a1ddbbd0486e803db7f43470a812e
Ivan Grachev <ivan@ivan.lol>
2023-03-21 23:19:00 +0300

    Initial Commit

---
+++ super-metal-mons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  super-metal-mons
+//
+//  Created by Ivan Grachev on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
