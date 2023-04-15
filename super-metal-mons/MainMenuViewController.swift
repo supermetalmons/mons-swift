233f8f53db7622226bd86b41c7ebe2d1697ea4d1
Ivan Grachev <ivan@ivan.lol>
2023-04-15 23:06:12 +0300

    update main menu

--- super-metal-mons/MainMenuViewController.swift
+++ super-metal-mons/MainMenuViewController.swift
@@ -4,10 +4,20 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
     
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
     @IBAction func playButtonTapped(_ sender: Any) {
         let gameViewController = instantiate(MonsboardViewController.self)
         gameViewController.modalPresentationStyle = .overFullScreen
         present(gameViewController, animated: false)
+    }
+    
+    @IBAction func joinButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "there is no link", message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: Strings.ok, style: .default) { _ in }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
     
 }
---
+++ super-metal-mons/Strings.swift
@@ -0,0 +1,10 @@
+// Copyright © 2023 super metal mons. All rights reserved.
+
+import Foundation
+
+struct Strings {
+    
+    static let ok = "OK"
+    static let cancel = "Cancel"
+    
+}
