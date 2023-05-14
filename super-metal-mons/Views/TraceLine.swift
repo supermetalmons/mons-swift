1cd42d7ff9ecc205d4746bd0422cda5d81736f01
Ivan Grachev <ivan@ivan.lol>
2023-05-14 21:52:03 +0300

    add trace line model

---
+++ super-metal-mons/Views/TraceLine.swift
@@ -0,0 +1,10 @@
+// Copyright © 2023 super metal mons. All rights reserved.
+
+import UIKit
+
+struct TraceLine {
+    let from: CGPoint
+    let to: CGPoint
+    let color: UIColor
+    let proportionalWidth: CGFloat
+}
