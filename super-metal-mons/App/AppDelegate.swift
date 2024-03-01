8a54b95190abc450441a8e48306cf651dcefd551
ivan grachev <ivan@ivan.lol>
2024-03-01 22:36:48 +0300

    read codes from csv

--- super-metal-mons/App/AppDelegate.swift
+++ super-metal-mons/App/AppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Firebase.setup()
         Audio.shared.prepare()
@@ -15,4 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    private func loadClaimCodes() -> [String] {
+        guard let path = Bundle.main.path(forResource: "links", ofType: "csv"),
+              let data = try? String(contentsOfFile: path, encoding: .utf8) else { return [] }
+        let rows = data.components(separatedBy: "\n")
+        var claimCodes: [String] = []
+        for (index, row) in rows.enumerated() {
+            if index == 0 || row.isEmpty { continue }
+            let columns = row.components(separatedBy: ",")
+            if columns.count > 4 {
+                let claimCode = columns[4]
+                claimCodes.append(claimCode)
+            }
+        }
+        return claimCodes
+    }
+    
 }
