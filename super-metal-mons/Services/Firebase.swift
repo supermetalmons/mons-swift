8b7bc9fa1b8b22709981ef935617d250a8a0196a
ivan grachev <ivan@ivan.lol>
2024-03-01 18:19:24 +0300

    implement claim

--- super-metal-mons/Services/Firebase.swift
+++ super-metal-mons/Services/Firebase.swift
@@ -31,20 +31,21 @@ class Firebase: BaseFirebase {
     static func setup() {
         AppCheck.setAppCheckProviderFactory(MonsAppCheckProviderFactory())
         baseSetup()
-        Task {
-            await checkFirestore()
-        }
     }
     
-    static func checkFirestore() async {
+    static func claim(completion: @escaping (String?) -> Void) {
         let db = Firestore.firestore()
-        do {
-            let snapshot = try await db.collection("test").getDocuments()
-            for document in snapshot.documents {
-                NSLog("\(document.documentID) => \(document.data())")
+        db.collection("items").whereField("claimed", isEqualTo: false).limit(to: 1).getDocuments { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents, let documentToClaim = documents.first else { return }
+            db.runTransaction({ (transaction, errorPointer) -> Any? in
+                let itemRef = documentToClaim.reference
+                guard let itemDocument = try? transaction.getDocument(itemRef) else { return nil }
+                guard let claimed = itemDocument.data()?["claimed"] as? Bool, !claimed else { return nil }
+                transaction.updateData(["claimed": true], forDocument: itemRef)
+                return itemDocument.data()?["code"] as? String
+            }) { (result, _) in
+                completion(result as? String)
             }
-        } catch {
-            NSLog("Error getting documents: \(error)")
         }
     }
     
