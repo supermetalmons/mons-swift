c4468a6cfec634db48e832410d0e16835b9236ee
ivan grachev <ivan@ivan.lol>
2024-04-08 16:12:22 +0300

    join game deeplink

--- super-metal-mons/Extensions/URL.swift
+++ super-metal-mons/Extensions/URL.swift
@@ -6,6 +6,7 @@ extension URL {
     
     static let baseMonsLink = "mons.link"
     static let baseMonsRehab = "mons.rehab"
+    static let supermonsScheme = "supermons"
     
     static func forGame(id: String) -> String {
         return baseMonsLink + "/" + id
@@ -20,18 +21,20 @@ extension URL {
             link = absoluteString
         }
         
-        let prefix = URL.baseMonsLink + "/"
+        let prefixes = [URL.baseMonsLink + "/", "?play="]
         
-        if link.hasPrefix(prefix), link.count > prefix.count {
-            let id = String(link.dropFirst(prefix.count))
-            return id
-        } else {
-            return nil
+        for prefix in prefixes {
+            if link.hasPrefix(prefix), link.count > prefix.count {
+                let id = String(link.dropFirst(prefix.count))
+                return id
+            }
         }
+        
+        return nil
     }
     
     var secretAppRequest: SecretAppRequest? {
-        guard (host == URL.baseMonsRehab && lastPathComponent == "app-request") || scheme == "supermons",
+        guard (host == URL.baseMonsRehab && lastPathComponent == "app-request") || scheme == URL.supermonsScheme,
               let components = URLComponents(url: self, resolvingAgainstBaseURL: false),
               let queryItems = components.queryItems else { return nil }
         var dict = [String: String]()
